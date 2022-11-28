42e0177b65edbb31dc1d891628b67d0de242ae80
Filip Lobpreis <f.lobpreis@gmail.com>
2022-11-28 18:36:56 +0100

    zadanie4
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

---
+++ src/trajectory_visualization/src/trajectory_visualization.cpp
@@ -0,0 +1,125 @@
+#include <ros/ros.h>
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <moveit_msgs/DisplayTrajectory.h>
+#include <cmath>
+#include <fstream>
+
+/**
+ * Vypocitajte koeficienty a;
+ *
+ * q = f(a)
+ * dq = f(a)
+ * 
+ * zapis do subory:
+ *  q(t) => q(0), q(0.1), ...
+ *  dq(t) => dq(0), dq(0.1), ...
+ *
+ * zapis do ros msg moveit_msg::DisplayTrajectory
+ *
+ * point.positions[0] = f(a)
+ *
+ * publisher.publish()
+ *
+ */
+
+#define t0 0
+#define t1 1
+#define t2 4
+
+void example(int argc, char **argv);
+
+int main(int argc, char **argv)
+{
+	Eigen::MatrixXd pointOne(6, 6);
+	Eigen::MatrixXd pointFinal(6, 1);
+	Eigen::MatrixXd pointOneAParams(6, 1);
+	pointOne << 1,   t0, std::pow(t0, 2),	 std::pow(t0, 3),	 std::pow(t0, 4),	 std::pow(t0, 5),
+				0, 2*t0, std::pow(t0, 2),  3*std::pow(t0, 2),  4*std::pow(t0, 3),  5*std::pow(t0, 4),
+				0,	  0,			   1,				6*t0, 12*std::pow(t0, 2), 20*std::pow(t0, 3),
+				1,   t2, std::pow(t2, 2),	 std::pow(t2, 3),	 std::pow(t2, 4),	 std::pow(t2, 5),
+				0, 2*t2, std::pow(t2, 2),  3*std::pow(t2, 2),  4*std::pow(t2, 3),  5*std::pow(t2, 4),
+				0,    0,			   1,				6*t2, 12*std::pow(t2, 2), 20*std::pow(t2, 3);
+	pointFinal << 0, 0, 0, 90, 0, 0;
+
+	pointOneAParams = pointOne.inverse() * pointFinal;
+}
+
+void example(int argc, char **argv)
+{
+	// Vytvorenie node a publishera
+	ros::init(argc, argv, "trajectory_visualization");
+	ros::NodeHandle n;
+	ros::Publisher publisher = n.advertise<moveit_msgs::DisplayTrajectory>("trajectory", 1);
+
+	// Sprava pre trajektoriu
+	moveit_msgs::RobotTrajectory trajectory;
+	// Mena klbov musia byt vyplnene
+	trajectory.joint_trajectory.joint_names = {"joint_1", "joint_2", "joint_3", "joint_4", "joint_5", "joint_6"};
+
+	// V cykle sa vytvori trajektoria, kde pre ukazku kazdy klb bude mat hodnota q(t) = t*0.5
+	for (double t = 0; t <= 2; t += 0.1) {
+
+		// Vytvorenie prejazdoveho bodu
+		trajectory_msgs::JointTrajectoryPoint point;
+
+		// Robot ma 6 klbov
+		point.positions.resize(6);
+		point.velocities.resize(6);
+		point.accelerations.resize(6);
+
+		// Klb 1
+		point.positions[0] = t*0.5;
+		point.velocities[0] = 0;
+		point.accelerations[0] = 0;
+
+		// Klb 2
+		point.positions[1] = t*0.5;
+		point.velocities[1] = 0;
+		point.accelerations[1] =0;
+
+		// Klb 3
+		point.positions[2] = t*0.5;
+		point.velocities[2] = 0;
+		point.accelerations[2] = 0;
+
+		// Klb 4
+		point.positions[3] = t*0.5;
+		point.velocities[3] = 0;
+		point.accelerations[3] =0;
+
+		// Klb 5
+		point.positions[4] = t*0.5;
+		point.velocities[4] = 0;
+		point.accelerations[4] = 0;
+
+		// Klb 6
+		point.positions[5] = t*0.5;
+		point.velocities[5] = 0;
+		point.accelerations[5] =0;
+
+		// Vlozenie casu prejazdu
+		point.time_from_start = ros::Duration(t);
+
+		// VLozenie bodu do trajektorie
+		trajectory.joint_trajectory.points.push_back(point);
+	}
+
+	// Sprava pre vizualizaciu
+	moveit_msgs::DisplayTrajectory display_trajectory;
+
+	// Potrebne vyplnit nazov modelu
+	display_trajectory.model_id = "abb_irb";
+
+	// Vlozenie vypocitanej trajektorie
+	display_trajectory.trajectory.push_back(trajectory);
+
+	// Publikuj trajektoriu kazde 2 sekundy
+	ros::Rate loop_rate(0.5);
+	while (ros::ok()) {
+		publisher.publish(display_trajectory);
+		loop_rate.sleep();
+	}
+}
+
